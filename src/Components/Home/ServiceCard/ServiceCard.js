322eb336be692747640259f26b78a3fae8f34841
MD Riyajul Kabir Rakib <mdriyajulkabir@gmail.com>
2022-01-02 00:35:17 +0600

    change in index file

---
+++ src/Components/Home/ServiceCard/ServiceCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ServiceCard = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-12">
+                        
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-12"></div>
+                    <div className="col-lg-4 col-md-4 col-sm-12"></div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ServiceCard;
