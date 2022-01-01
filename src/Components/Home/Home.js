3e0dd21bee2da29b4438be9c2e63195625815746
MD Riyajul Kabir Rakib <mdriyajulkabir@gmail.com>
2022-01-02 00:02:27 +0600

    Added office benifit section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header/Header';
+import OfficeBenifits from './OfficeBenifits/OfficeBenifits';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <OfficeBenifits/>
         </div>
     );
 };
---
+++ src/Components/Home/OfficeBenifits/OfficeBenifits.js
@@ -0,0 +1,77 @@
+import React from "react";
+import painter from "../../../Images/painterPeople.png";
+
+const OfficeBenifits = () => {
+  return (
+    <>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-lg-4 col-md-4 col-sm-12 mb-4">
+            <div className="row">
+              <div className="col-2">
+                <i
+                  style={{ color: "#fda12b" }}
+                  class="far fa-clock fs-1 pt-3"></i>
+              </div>
+              <div className="col-10 text-start">
+                <h3>Office 24/7 Opened</h3>
+                <p>
+                  Boldman is also likely to be focused on slow and gentle
+                  movements so it’s a great type of steps to..
+                </p>
+                <a
+                  href="#benifits"
+                  style={{
+                    textDecoration: "none",
+                    color: "#fda12b",
+                    fontWeight: "bold",
+                  }}>
+                  GET A FREE QUOTE &gt;&gt;
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-12 mb-4">
+            <div className="row">
+              <div className="col-2">
+                <i
+                  style={{ color: "#fda12b" }}
+                  className="fas fa-plug fs-1 pt-3"></i>
+              </div>
+              <div className="col-10 text-start">
+                <h3>Our Installations</h3>
+                <p>
+                  Boldman is also likely to be focused on slow and gentle
+                  movements so it’s a great type of steps to..
+                </p>
+                <a
+                  href="#benifits"
+                  style={{
+                    textDecoration: "none",
+                    color: "#fda12b",
+                    fontWeight: "bold",
+                  }}>
+                  GET A FREE QUOTE &gt;&gt;
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-12 ">
+            <div className="row">
+              <div className="col-2">
+                <i style={{ color: "#fda12b" }} class="fas fa-users-cog fs-1 pt-3"></i>
+              </div>
+              <div className="col-10 text-start">
+                <h3>Our Services</h3>
+                <p>Boldman is also likely to be focused on slow.</p>
+                <img src={painter} className="img-fluid" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OfficeBenifits;
