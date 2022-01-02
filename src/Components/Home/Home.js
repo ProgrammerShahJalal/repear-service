de2c6a9f99c94e608caff38bafe0c593032b693f
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-01-02 13:49:19 +0600

    rakid service card

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Footer from '../Shared/Footer/Footer';
 import Header from '../Shared/Header/Header';
+import OfficeBenifits from './OfficeBenifits/OfficeBenifits';
+import ServiceCard from './ServiceCard/ServiceCard';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <OfficeBenifits></OfficeBenifits>
+            <ServiceCard></ServiceCard>
             <Footer></Footer>
         </div>
     );
