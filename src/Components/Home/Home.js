9146da75db76ff9f50a45d12aca4f375bb08f458
MD Riyajul Kabir Rakib <mdriyajulkabir@gmail.com>
2022-01-02 12:41:57 +0600

    design service card section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './Header/Header';
 import OfficeBenifits from './OfficeBenifits/OfficeBenifits';
+import ServiceCard from './ServiceCard/ServiceCard';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <OfficeBenifits/>
+
+            <ServiceCard/>
         </div>
     );
 };
--- src/Components/Home/ServiceCard/ServiceCard.js
+++ src/Components/Home/ServiceCard/ServiceCard.js
@@ -1,15 +1,34 @@
 import React from 'react';
-
+import homeMaintain from '../../../Images/home.png'
+import garden from '../../../Images/mower.png'
+import waterDrop from '../../../Images/faucet.png'
+import './ServiceCard.css'
 const ServiceCard = () => {
     return (
         <>
             <div className="container">
-                <div className="row">
-                    <div className="col-lg-4 col-md-4 col-sm-12">
-                        
+                <div className="row my-5">
+                    <div className="col-lg-4 col-md-4 col-sm-12 px-4 my-3">
+                        <div className='serviceCard'>
+                        <img src={homeMaintain} className='w-25' alt="" />
+                        <h3>ʜᴏᴍᴇ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ</h3>
+                        <p>We have experience in home maintenance any surface from new construct.</p>
+                        </div>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm-12"></div>
-                    <div className="col-lg-4 col-md-4 col-sm-12"></div>
+                    <div className="col-lg-4 col-md-4 col-sm-12 px-4 my-3">
+                        <div className="serviceCard">
+                        <img src={garden} className='w-25' alt="" />
+                        <h3>🇬‌🇦‌🇷‌🇩‌🇪‌🇳‌ 🇸‌🇪‌🇷‌🇻‌🇮‌🇨‌🇪‌🇸‌</h3>
+                        <p>We have experience in home maintenance any surface from new construct.</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-12 px-4 my-3">
+                        <div className="serviceCard">
+                        <img src={waterDrop} className='w-25' alt="" />
+                        <h3>🇵‌🇱‌🇺‌🇲‌🇧‌🇮‌🇳‌🇬‌ 🇸‌🇪‌🇷‌🇻‌🇮‌🇨‌🇪‌🇸‌</h3>
+                        <p>We have experience in home maintenance any surface from new construct.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
