d9a566ec77ab812eb266db9193e5d8e66a437888
hrid620 <hridishere@gmail.com>
2022-01-02 13:59:21 +0600

    services added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Services from '../Services/Services';
 import Footer from '../Shared/Footer/Footer';
 import Header from '../Shared/Header/Header';
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Services></Services>
             <Footer></Footer>
         </div>
     );
