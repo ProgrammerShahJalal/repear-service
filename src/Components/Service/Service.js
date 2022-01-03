6d67c319d0024e76a28a4d967eb243d640592269
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-01-03 14:10:58 +0600

    service card make perfect

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -13,7 +13,7 @@ const Service = (props) => {
                     <img src={image} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{name}</h5>
-                        <p className="card-text"> <strong>Description:</strong> {description}</p>
+                        <p className="card-text"> {description}</p>
                         <p className="text-nowrap"><strong >Price: $</strong>{price}</p>
 
                         <Link to={`/buynow/${_id}`}>
